95752a162d8244eb11e502afab28fe95bbcffcb3
Tom Christie <tom@tomchristie.com>
2017-04-13 16:02:27 +0100

    Add test for unhandled exceptions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -3,21 +3,33 @@ from apistar.exceptions import APIException
 from apistar.test import TestClient
 
 
-def exc():
+def handled_exception():
     raise APIException(message='error', status_code=400)
 
 
+def unhandled_exception():
+    raise Exception('Oh noes!')
+
+
 app = App(routes=[
-    Route('/exc/', 'GET', exc),
+    Route('/handled_exception/', 'GET', handled_exception),
+    Route('/unhandled_exception/', 'GET', unhandled_exception),
 ])
 
 
 client = TestClient(app)
 
 
-def test_exc():
-    response = client.get('/exc/')
+def test_handled_exception():
+    response = client.get('/handled_exception/')
     assert response.status_code == 400
     assert response.json() == {
         'message': 'error'
     }
+
+
+def test_unhandled_exception():
+    response = client.get('/unhandled_exception/')
+    assert response.status_code == 500
+    assert response.headers['Content-Type'] == 'text/plain; charset=utf-8'
+    assert 'Traceback' in response.text
