a7ddfb0b047ac3375e53b671905b611933016054
Tom Christie <tom@tomchristie.com>
2017-04-20 12:56:54 +0100

    Add a 'Template' component

--- apistar/templating.py
+++ apistar/templating.py
@@ -1,12 +1,26 @@
-from jinja2 import Environment, FileSystemLoader
+import os
 
+import jinja2
+
+from apistar.pipelines import ArgName
 from apistar.settings import Settings
 
 
-class Templates(Environment):
+class Templates(jinja2.Environment):
     @classmethod
     def build(cls, settings: Settings):
         template_dir = settings['TEMPLATES']['TEMPLATE_DIR']
         return Templates(
-            loader=FileSystemLoader(template_dir)
+            loader=jinja2.FileSystemLoader(template_dir)
         )
+
+
+class Template(jinja2.Template):
+    suffix = '.html'
+    path_delimiter = '__'
+
+    @classmethod
+    def build(cls, arg_name: ArgName, templates: Templates):
+        paths = arg_name.split(cls.path_delimiter)
+        path = os.path.join(*paths) + cls.suffix
+        return templates.get_template(path)
--- tests/test_templating.py
+++ tests/test_templating.py
@@ -4,25 +4,30 @@ import tempfile
 from apistar.app import App
 from apistar.routing import Route
 from apistar.settings import Settings
-from apistar.templating import Templates
+from apistar.templating import Template, Templates
 from apistar.test import TestClient
 
 
-def render_template(templates: Templates):
+def get_and_render_template(username: str, templates: Templates):
     index = templates.get_template('index.html')
-    return index.render()
+    return index.render(username=username)
+
+
+def render_template(username: str, index: Template):
+    return index.render(username=username)
 
 
 routes = [
-    Route('/', 'GET', render_template),
+    Route('/get_and_render_template/', 'GET', get_and_render_template),
+    Route('/render_template/', 'GET', render_template),
 ]
 
 
-def test_render_template():
+def test_get_and_render_template():
     with tempfile.TemporaryDirectory() as tempdir:
         path = os.path.join(tempdir, 'index.html')
         with open(path, 'w') as index:
-            index.write('<html><body>Hello, world</body><html>')
+            index.write('<html><body>Hello, {{ username }}</body><html>')
 
         settings = Settings(
             TEMPLATES={
@@ -31,7 +36,26 @@ def test_render_template():
         )
         app = App(routes=routes, settings=settings)
         client = TestClient(app)
-        response = client.get('/')
+        response = client.get('/get_and_render_template/?username=tom')
 
         assert response.status_code == 200
-        assert response.text == '<html><body>Hello, world</body><html>'
+        assert response.text == '<html><body>Hello, tom</body><html>'
+
+
+def test_render_template():
+    with tempfile.TemporaryDirectory() as tempdir:
+        path = os.path.join(tempdir, 'index.html')
+        with open(path, 'w') as index:
+            index.write('<html><body>Hello, {{ username }}</body><html>')
+
+        settings = Settings(
+            TEMPLATES={
+                'TEMPLATE_DIR': tempdir
+            }
+        )
+        app = App(routes=routes, settings=settings)
+        client = TestClient(app)
+        response = client.get('/render_template/?username=tom')
+
+        assert response.status_code == 200
+        assert response.text == '<html><body>Hello, tom</body><html>'
