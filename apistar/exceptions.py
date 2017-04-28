a1ddc35817ca050c4a26df0b209d765dbb8e39e1
Max Muth <kontakt@maxi-muth.de>
2017-04-29 00:42:04 +0200

    Exceptions: add line breaks to function signatures

--- apistar/exceptions.py
+++ apistar/exceptions.py
@@ -6,7 +6,8 @@ class ConfigurationError(Exception):
 
 
 class SchemaError(Exception):
-    def __init__(self, detail: Union[str, dict]) -> None:
+    def __init__(self,
+                 detail: Union[str, dict]) -> None:
         self.detail = detail
         super().__init__(detail)
 
@@ -17,7 +18,9 @@ class APIException(Exception):
     default_status_code = 500
     default_detail = 'Server error'
 
-    def __init__(self, detail: Union[str, dict]=None, status_code: int=None) -> None:
+    def __init__(self,
+                 detail: Union[str, dict]=None,
+                 status_code: int=None) -> None:
         self.detail = self.default_detail if (detail is None) else detail
         self.status_code = self.default_status_code if (status_code is None) else status_code
 
@@ -26,7 +29,10 @@ class Found(APIException):
     default_status_code = 302
     default_detail = ''
 
-    def __init__(self, location: str, detail: Union[str, dict]=None, status_code: int=None) -> None:
+    def __init__(self,
+                 location: str,
+                 detail: Union[str, dict]=None,
+                 status_code: int=None) -> None:
         self.location = location
         super().__init__(detail, status_code)
 
