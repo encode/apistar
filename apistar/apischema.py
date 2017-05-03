5f102eb4b07d13071a11cfcef96e26050ba46c2e
Tom Christie <tom@tomchristie.com>
2017-05-03 12:53:51 +0100

    First pass at generating API Schemas

---
+++ apistar/apischema.py
@@ -0,0 +1,51 @@
+import inspect
+
+from uritemplate import URITemplate
+
+from apistar import http, schema
+from apistar.app import App
+from apistar.routing import primitive_types, schema_types
+from coreapi import Document, Field, Link
+
+
+class APISchema(Document):
+    @classmethod
+    def build(cls, app: App):
+        content = {}
+
+        for (path, method, view) in app.routes:
+            view_signature = inspect.signature(view)
+            uritemplate = URITemplate(path)
+            name = view.__name__
+
+            fields = []
+            for param in view_signature.parameters.values():
+
+                if param.annotation is inspect.Signature.empty:
+                    annotated_type = str
+                else:
+                    annotated_type = param.annotation
+
+                location = None
+                required = False
+                if param.name in uritemplate.variable_names:
+                    location = 'path'
+                    required = True
+                elif (annotated_type in primitive_types) or issubclass(annotated_type, schema_types):
+                    if method in ('POST', 'PUT', 'PATCH'):
+                        if issubclass(annotated_type, schema.Object):
+                            location = 'body'
+                            required = True
+                        else:
+                            location = 'form'
+                    else:
+                        location = 'query'
+
+                if location is not None:
+                    field = Field(name=param.name, location=location, required=required)
+                    fields.append(field)
+
+            link = Link(url=path, action=method, fields=fields)
+            content[name] = link
+
+        return cls(content=content)
---
+++ tests/test_apischema.py
@@ -0,0 +1,70 @@
+from coreapi import Field, Link
+
+from apistar import schema
+from apistar.app import App
+from apistar.apischema import APISchema
+from apistar.routing import Route
+
+
+class ToDoNote(schema.Object):
+    properties = {
+        'id': schema.Integer(min_value=0),
+        'text': schema.String(max_length=100),
+        'complete': schema.Boolean
+    }
+
+
+def list_todo(app: App, search):  # pragma: nocover
+    pass
+
+
+def add_todo(note: ToDoNote):  # pragma: nocover
+    pass
+
+
+def show_todo(ident: int):  # pragma: nocover
+    pass
+
+
+def set_complete(ident: int, complete: bool):  # pragma: nocover
+    pass
+
+
+routes = [
+    Route('/todo/', 'GET', list_todo),
+    Route('/todo/', 'POST', add_todo),
+    Route('/todo/{ident}/', 'GET', show_todo),
+    Route('/todo/{ident}/', 'PUT', set_complete),
+]
+
+app = App(routes=routes)
+
+
+def test_api_schema():
+    schema = APISchema.build(app)
+    expected = APISchema(content={
+        'list_todo': Link(
+            url='/todo/',
+            action='GET',
+            fields=[Field(name='search', location='query', required=False)]
+        ),
+        'add_todo': Link(
+            url='/todo/',
+            action='POST',
+            fields=[Field(name='note', location='body', required=True)]
+        ),
+        'show_todo': Link(
+            url='/todo/{ident}/',
+            action='GET',
+            fields=[Field(name='ident', location='path', required=True)]
+        ),
+        'set_complete': Link(
+            url='/todo/{ident}/',
+            action='PUT',
+            fields=[
+                Field(name='ident', location='path', required=True),
+                Field(name='complete', location='form', required=False)
+            ]
+        )
+    })
+    assert schema == expected
