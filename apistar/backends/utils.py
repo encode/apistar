b8fc507a5b57fe6a890f85f7a7e0c67d72f59d2e
Adam Hopkins <adam@optymizer.com>
2017-05-09 13:26:18 +0300

    Capitalize key

--- apistar/backends/utils.py
+++ apistar/backends/utils.py
@@ -6,7 +6,7 @@ default_db_backend = 'apistar.backends.sqlalchemy.SQLAlchemy'
 
 def db_backend_loader(settings):
     database_settings = settings.get('DATABASE', {})
-    backend_name = database_settings.get('backend', default_db_backend)
+    backend_name = database_settings.get('BACKEND', default_db_backend)
 
     parts = backend_name.split('.')
     class_name = parts.pop()
