8fefa6a4f347bb069df47c2ae9148e58a7c4e8bd
Sam Bolgert <sbolgert@gmail.com>
2017-05-16 21:33:42 -0700

    Make Django default instead of SQLAlchemy
    This commit shows current hardcoded usages of SQLAlchemy that need to be
    abstracted.

--- apistar/commands/create_tables.py
+++ apistar/commands/create_tables.py
@@ -6,8 +6,8 @@ def create_tables():
     Create SQLAlchemy tables.
     """
     from apistar.main import get_current_app
-    from apistar.backends import SQLAlchemy
+    from apistar.backends import DjangoBackend
     app = get_current_app()
-    db_backend = SQLAlchemy.build(settings=app.settings)
+    db_backend = DjangoBackend.build(settings=app.settings)
     db_backend.create_tables()
     click.echo("Tables created")
