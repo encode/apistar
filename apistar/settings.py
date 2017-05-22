c475537dc0c8a59554ff20ee634f6de407fde973
Michael Dougherty <maackle.d@gmail.com>
2017-05-22 16:58:05 -0700

    Use `@builder` for settings module

--- apistar/settings.py
+++ apistar/settings.py
@@ -5,9 +5,6 @@ from apistar.core import ArgName
 
 
 class Settings(dict):
-    @classmethod
-    def build(cls, app: app.App):
-        return cls(app.settings)
 
     def get(self, indexes: Union[str, List[str]], default: Any=None) -> Any:
         if isinstance(indexes, str):
@@ -21,11 +18,17 @@ class Settings(dict):
         return value
 
 
+@app.builder
+def build_settings(app: app.App) -> Settings:
+    return Settings(app.settings)
+
+
 class Setting(object):
     def __new__(cls, *args):
         assert len(args) == 1
         return args[0]
 
-    @classmethod
-    def build(cls, arg_name: ArgName, settings: Settings):
-        return settings.get(arg_name)
+
+@app.builder
+def build(arg_name: ArgName, settings: Settings) -> Setting:
+    return settings.get(arg_name)
