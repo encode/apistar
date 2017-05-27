ab806d6f7a2406eeb8571da927be81367d6a8b0c
Jesse Riggins <jesse@jr4consulting.com>
2017-05-27 00:43:56 -0500

    Add linter exclusions
    To the imports in apistar/backends/__init__.py

--- apistar/backends/__init__.py
+++ apistar/backends/__init__.py
@@ -1,14 +1,13 @@
 __all__ = []
 
 try:
-    from apistar.backends.django_backend import DjangoBackend
+    from apistar.backends.django_backend import DjangoBackend  # noqa
     __all__.append('DjangoBackend')
 except ImportError:
     pass
 
 try:
-    from apistar.backends.sqlalchemy_backend import SQLAlchemy
+    from apistar.backends.sqlalchemy_backend import SQLAlchemy  # noqa
     __all__.append('SQLAlchemy')
 except ImportError:
     pass
-
