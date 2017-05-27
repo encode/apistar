927f20aa787c71784c07269d44a3d1c279e319c1
Jesse Riggins <jesse@jr4consulting.com>
2017-05-26 23:10:48 -0500

    Fix issue causing projects without Django to fail.
    If project is using SQLAlchemy and not Django ORM, the system fails with
    an error.

--- apistar/backends/__init__.py
+++ apistar/backends/__init__.py
@@ -1,4 +1,14 @@
-from apistar.backends.django_backend import DjangoBackend
-from apistar.backends.sqlalchemy_backend import SQLAlchemy
+__all__ = []
 
-__all__ = ['SQLAlchemy', 'DjangoBackend']
+try:
+    from apistar.backends.django_backend import DjangoBackend
+    __all__.append('DjangoBackend')
+except ImportError:
+    pass
+
+try:
+    from apistar.backends.sqlalchemy_backend import SQLAlchemy
+    __all__.append('SQLAlchemy')
+except ImportError:
+    pass
+
