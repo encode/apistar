898ca2a8bbf6644d4efa8da0896e0c1df48b9fbd
Jesse Riggins <jesse@jr4consulting.com>
2017-05-28 00:33:22 -0500

    Attempt to make changes look more like compat.py

--- apistar/backends/__init__.py
+++ apistar/backends/__init__.py
@@ -1,13 +1,12 @@
-__all__ = []
+# flake8: noqa
 
 try:
-    from apistar.backends.django_backend import DjangoBackend  # noqa
-    __all__.append('DjangoBackend')
-except ImportError:
-    pass
+    from apistar.backends.django_backend import DjangoBackend
+except ImportError:  # pragma: no cover
+    DjangoBackend = None  # type: ignore
+
 
 try:
-    from apistar.backends.sqlalchemy_backend import SQLAlchemy  # noqa
-    __all__.append('SQLAlchemy')
-except ImportError:
-    pass
+    from apistar.backends.sqlalchemy_backend import SQLAlchemy
+except ImportError:  # pragma: no cover
+    SQLAlchemy = None  # type: ignore
