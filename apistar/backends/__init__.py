d1b5eb8b7fa2c9e824df5cd09a9f5f7d8d517462
Omer Katz <omer.drow@gmail.com>
2017-07-20 16:26:53 +0300

    Make flake8 happy.

--- apistar/backends/__init__.py
+++ apistar/backends/__init__.py
@@ -1,14 +1,14 @@
 importable_backends = []
 
 try:
-    from apistar.backends.django_backend import DjangoBackend
+    from apistar.backends.django_backend import DjangoBackend  # noqa
 except ImportError:
     pass
 else:
     importable_backends.append('DjangoBackend')
 
 try:
-    from apistar.backends.sqlalchemy_backend import SQLAlchemy
+    from apistar.backends.sqlalchemy_backend import SQLAlchemy  # noqa
 except ImportError:
     pass
 else:
