9f7d806efb9a24ce0491fe95aed3a7245f5cff1d
Tom Christie <tom@tomchristie.com>
2017-08-24 11:23:28 +0100

    Version 0.2.2

--- apistar/__init__.py
+++ apistar/__init__.py
@@ -10,7 +10,7 @@ from apistar.http import Response
 from apistar.test import TestClient
 from apistar.types import Settings
 
-__version__ = '0.2.1'
+__version__ = '0.2.2'
 __all__ = [
     'Command', 'Component', 'Response', 'Route', 'Include', 'Settings',
     'TestClient'
