d396a73a0dd28ade2e75640a974a6f31a6fc4463
Tom Christie <tom@tomchristie.com>
2017-09-20 13:01:35 +0100

    Version 0.3.4

--- apistar/__init__.py
+++ apistar/__init__.py
@@ -10,7 +10,7 @@ from apistar.http import Response
 from apistar.test import TestClient
 from apistar.types import Settings
 
-__version__ = '0.3.3'
+__version__ = '0.3.4'
 __all__ = [
     'annotate', 'Command', 'Component', 'Response', 'Route', 'Include',
     'Settings', 'TestClient'
