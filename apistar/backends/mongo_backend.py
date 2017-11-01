c122f8c297fab0f085d696b04a12bd1519f8731d
Humberto Rocha <humrochagf@gmail.com>
2017-11-01 01:01:53 -0200

    Override decorated class constructor to mount _id and expose ObjectId as id property at class instance

--- apistar/backends/mongo_backend.py
+++ apistar/backends/mongo_backend.py
@@ -1,24 +1,46 @@
 import contextlib
 import typing
 
-from apistar import Command, Component, Settings
+from bson.objectid import ObjectId
 from pymongo import MongoClient
+
+from apistar import Command, Component, Settings
 
 registered_collections = []
 
 
 def collection(cls):
+    """
+    Decorator to register apistar custom Objects as mongodb collections
+    """
+
     registered_collections.append({
         'collection': cls.__name__.lower(),
         'class': cls,
     })
 
+    def __init__(self, *args, **kwargs):
+        if len(args) == 1 and not kwargs and isinstance(args[0], dict):
+            id = args[0].pop('_id', None)
+        else:
+            id = kwargs.pop('_id', None)
+
+        if id:
+            self['_id'] = id
+
+        super(cls, self).__init__(*args, **kwargs)
+
     def __setitem__(self, index, value):
         if index == '_id':
-            value = str(value)
+            if type(value) is ObjectId:
+                self.id = value
+                value = str(value)
+            else:
+                self.id = ObjectId(value)
 
         super(cls, self).__setitem__(index, value)
 
+    cls.__init__ = __init__
     cls.__setitem__ = __setitem__
 
     return cls
