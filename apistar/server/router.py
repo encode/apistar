f9cecff55a1248de246fa23e813f05c81e9714cf
Tom Christie <tom@tomchristie.com>
2018-03-13 10:12:28 +0000

    Fix instantiation of router cache

--- apistar/server/router.py
+++ apistar/server/router.py
@@ -32,7 +32,7 @@ class Router():
         self.name_lookups = name_lookups
 
         # Use an MRU cache for router lookups.
-        self._lookup_cache = dict_type
+        self._lookup_cache = dict_type()
         self._lookup_cache_size = 10000
 
     def get_converter(self, field):
