8ede3773af3d34b3352f4a9560efed306d8ca05e
Tom Christie <tom@tomchristie.com>
2018-03-14 15:50:22 +0000

    Include fields in schema test

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -1,27 +1,29 @@
-from apistar import App, Document, Link, TestClient, http
+from apistar import App, Document, Field, Link, TestClient, http
 from apistar.codecs import OpenAPICodec
 from apistar.server.handlers import serve_schema
 
 
-def hello_world():
-    return http.Response({'hello': 'world'})
+def hello_world(username: http.QueryParam):
+    return http.Response({'hello': username or 'world'})
 
 
 document = Document(
     title='API Star',
     content=[
-        Link(url='/hello-world/', method='GET', handler=hello_world),
+        Link(url='/hello-world/', method='GET', handler=hello_world, fields=[
+            Field(name='username', location='query')
+        ]),
         Link(url='/schema/', method='GET', handler=serve_schema),
     ]
 )
 app = App(document)
-client = TestClient(app)
+test_client = TestClient(app)
 
 
 def test_get_schema():
-    response = client.get('/schema/')
+    response = test_client.get('/schema/')
     assert response.status_code == 200
     codec = OpenAPICodec()
-    document = codec.decode(response.content)
+    decoded_document = codec.decode(response.content)
     assert document.title == 'API Star'
     assert len(document.get_links()) == 2
