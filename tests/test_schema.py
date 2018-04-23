d06f3bc02128d3dc0c289c19722532e5c02210ac
Tom Christie <tom@tomchristie.com>
2018-04-23 13:14:41 +0100

    Allow Type as an annotation on GET handlers

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -12,12 +12,17 @@ def get_endpoint(name: str, age: int=None):
     raise NotImplementedError()
 
 
+def get_endpoint_with_type(user: User):
+    raise NotImplementedError()
+
+
 def post_endpoint(user: User):
     raise NotImplementedError()
 
 
 routes = [
     Route(url='/get-endpoint/', method='GET', handler=get_endpoint),
+    Route(url='/get-endpoint-with-type/', method='GET', handler=get_endpoint_with_type),
     Route(url='/post-endpoint/', method='POST', handler=post_endpoint),
     Route(url='/schema/', method='GET', handler=serve_schema, documented=False),
 ]
@@ -43,6 +48,30 @@ expected_schema = """{
                         "in": "query",
                         "schema": {
                             "type": "string"
+                        }
+                    },
+                    {
+                        "name": "age",
+                        "in": "query",
+                        "schema": {
+                            "type": "integer",
+                            "default": null,
+                            "nullable": true
+                        }
+                    }
+                ]
+            }
+        },
+        "/get-endpoint-with-type/": {
+            "get": {
+                "operationId": "get_endpoint_with_type",
+                "parameters": [
+                    {
+                        "name": "name",
+                        "in": "query",
+                        "schema": {
+                            "type": "string",
+                            "maxLength": 100
                         }
                     },
                     {
