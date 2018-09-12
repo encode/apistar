0c31150b07dfee563554b993f021cab688eecd73
Tom Christie <tom@tomchristie.com>
2018-09-12 12:55:40 +0100

    Fixing up 'apistar docs'/'apistar validate'

--- apistar/main.py
+++ apistar/main.py
@@ -5,9 +5,9 @@ import click
 import jinja2
 
 import apistar
-from apistar import codecs
 from apistar.exceptions import ParseError, ValidationError
-from apistar.validate import validate
+from apistar.schemas import OpenAPI, Swagger
+from apistar.validate import validate as apistar_validate
 
 
 def static_url(filename):
@@ -19,78 +19,102 @@ def main():
     pass
 
 
-FORMAT_CHOICES = click.Choice(['json', 'yaml', 'config', 'jsonschema', 'openapi', 'swagger'])
+def _base_format_from_filename(filename):
+    base, extension = os.path.splitext(filename)
+    return {
+        '.json': 'json',
+        '.yml': 'yaml',
+        '.yaml': 'yaml'
+    }.get(extension)
+
+
+def _echo_error(exc, verbose=False):
+    if verbose:
+        # Verbose output style.
+        lines = content.splitlines()
+        for message in reversed(exc.messages):
+            error_str = ' ' * (message.position.column_no - 1)
+            error_str += '^ '
+            error_str += message.text
+            error_str = click.style(error_str, fg='red')
+            lines.insert(message.position.line_no, error_str)
+        for line in lines:
+            click.echo(line)
+
+        click.echo()
+    else:
+        # Compact output style.
+        for message in exc.messages:
+            pos = message.position
+            if message.code == 'required':
+                index = message.index[:-1]
+            else:
+                index = message.index
+            if index:
+                fmt = '* %s (At %s, line %d, column %d.)'
+                output = fmt % (message.text, index, pos.line_no, pos.column_no)
+                click.echo(output)
+            else:
+                fmt = '* %s (At line %d, column %d.)'
+                output = fmt % (message.text, pos.line_no, pos.column_no)
+                click.echo(output)
+
+    click.echo(click.style('✘ ', fg='red') + exc.summary)
+
+
+FORMAT_SCHEMA_CHOICES = click.Choice(['openapi', 'swagger'])
+FORMAT_ALL_CHOICES = click.Choice(['json', 'yaml', 'config', 'jsonschema', 'openapi', 'swagger'])
 BASE_FORMAT_CHOICES = click.Choice(['json', 'yaml'])
 
 
 @click.command()
 @click.argument('schema', type=click.File('rb'))
-@click.option('--format', type=FORMAT_CHOICES, required=True)
+@click.option('--format', type=FORMAT_ALL_CHOICES, required=True)
 @click.option('--base-format', type=BASE_FORMAT_CHOICES, default=None)
 @click.option('--verbose', '-v', is_flag=True, default=False)
-def cli_validate(schema, format, base_format, verbose):
+def validate(schema, format, base_format, verbose):
     content = schema.read()
-    base, extension = os.path.splitext(schema.name)
     if base_format is None:
-        base_format = {
-            '.json': 'json',
-            '.yml': 'yaml',
-            '.yaml': 'yaml'
-        }.get(extension)
+        base_format = _base_format_from_filename(schema.name)
 
     try:
-        validate(content, format, base_format)
+        value = apistar_validate(content, format=format, base_format=base_format)
     except (ParseError, ValidationError) as exc:
-        if verbose:
-            # Verbose output style.
-            lines = content.splitlines()
-            for message in reversed(exc.messages):
-                error_str = ' ' * (message.position.column_no - 1)
-                error_str += '^ '
-                error_str += message.text
-                error_str = click.style(error_str, fg='red')
-                lines.insert(message.position.line_no, error_str)
-            for line in lines:
-                click.echo(line)
+        _echo_error(exc, verbose=verbose)
+        return
 
-            click.echo()
-        else:
-            # Compact output style.
-            for message in exc.messages:
-                pos = message.position
-                if message.code == 'required':
-                    index = message.index[:-1]
-                else:
-                    index = message.index
-                if index:
-                    fmt = '* %s (At %s, line %d, column %d.)'
-                    output = fmt % (message.text, index, pos.line_no, pos.column_no)
-                    click.echo(output)
-                else:
-                    fmt = '* %s (At line %d, column %d.)'
-                    output = fmt % (message.text, pos.line_no, pos.column_no)
-                    click.echo(output)
-
-        click.echo(click.style('✘ ', fg='red') + exc.summary)
-
-    else:
-        success_summary = {
-            'json': 'Valid JSON',
-            'yaml': 'Valid YAML',
-            'config': 'Valid APIStar config.',
-            'jsonschema': 'Valid JSONSchema document.',
-            'openapi': 'Valid OpenAPI schema.',
-            'swagger': 'Valid Swagger schema.',
-        }[format]
-        click.echo(click.style('✓ ', fg='green') + success_summary)
+    success_summary = {
+        'json': 'Valid JSON',
+        'yaml': 'Valid YAML',
+        'config': 'Valid APIStar config.',
+        'jsonschema': 'Valid JSONSchema document.',
+        'openapi': 'Valid OpenAPI schema.',
+        'swagger': 'Valid Swagger schema.',
+    }[format]
+    click.echo(click.style('✓ ', fg='green') + success_summary)
 
 
 @click.command()
 @click.argument('schema', type=click.File('rb'))
-def docs(schema):
-    codec = codecs.OpenAPICodec()
+@click.option('--format', type=FORMAT_SCHEMA_CHOICES, required=True)
+@click.option('--base-format', type=BASE_FORMAT_CHOICES, default=None)
+@click.option('--verbose', '-v', is_flag=True, default=False)
+def docs(schema, format, base_format, verbose):
     content = schema.read()
-    document = codec.decode(content)
+    if base_format is None:
+        base_format = _base_format_from_filename(schema.name)
+
+    try:
+        value = apistar_validate(content, format=format, base_format=base_format)
+    except (ParseError, ValidationError) as exc:
+        _echo_error(exc, verbose=verbose)
+        return
+
+    decoder = {
+        'openapi': OpenAPI,
+        'swagger': Swagger
+    }[format]
+    document = decoder().load(value)
 
     loader = jinja2.PrefixLoader({
         'apistar': jinja2.PackageLoader('apistar', 'templates')
@@ -121,4 +145,4 @@ def docs(schema):
 
 
 main.add_command(docs)
-main.add_command(cli_validate, name='validate')
+main.add_command(validate)
---
+++ apistar/schemas/__init__.py
@@ -0,0 +1,2 @@
+from apistar.schemas.openapi import OpenAPI
+from apistar.schemas.swagger import Swagger
