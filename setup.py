9863d6d5a23c437249b76b4980bdeb91944a9c4d
amenezes <alexandre.fmenezes@gmail.com>
2020-02-18 10:41:23 -0300

    Added tox

--- setup.py
+++ setup.py
@@ -82,6 +82,7 @@ setup(
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: PyPy",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
